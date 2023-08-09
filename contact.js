fc747bba8846eb474365d8027ba4c458abeea7e7
Yusril Ahmad <yusril7680@gmail.com>
2023-08-09 11:47:16 +0700

    day-3

---
+++ contact.js
@@ -0,0 +1,17 @@
+//console.log("Hello World!");
+//alert("Selamat Pagi")
+//document.write("Hello World!"); 
+
+let nama ="Ahmad Yusril";
+let umur = 22;
+let isMuda = true;
+
+Nama saya Ahmad Yusril saya 22 tahun
+console.log("nama saya Ahmad Yusril saya 22 tahun");
+console.log(`nama saya ${nama} saya =${umur} tahun`);
+
+//let x = 50
+//let y = 20
+
+//let result = x + y;
+//console.log(result);
