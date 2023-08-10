818a9345cac8def370c8e5372f7c0373a2de5bc2
Yusril Ahmad <yusril7680@gmail.com>
2023-08-10 22:10:28 +0700

    day-4

--- js/blog.js
+++ js/blog.js
@@ -1,111 +1,4 @@
-// let namaSiswa1 = "Bagus";
-// let namaSiswa2 = "Guswandi";
-// let namaSiswa3 = "Arsya";
-
-// console.log(namaSiswa1);
-// console.log(namaSiswa2);
-// console.log(namaSiswa3);
-
-// Array
-// tabungan value kalian, dimana kalian bisa memiliki lebih dari satu value dalam satu variabel
-// let namaSiswa = ["Bagus", "Guswandi", "Arsya", 2023, true];
-// console.log(namaSiswa);
-// console.log(namaSiswa[1]);
-// console.log(namaSiswa[2]);
-
-// let nama = "Bagus";
-// let alamat = "Tangerang";
-// let umur = 24;
-
-// console.log(nama);
-// console.log(alamat);
-// console.log(umur);
-
-// Object
-// tabungan variabel kalian, dimana kalian bisa memiliki lebih dari satu key-value dalam satu variabel
-// contoh = nama: "Bagus", alamat: "Tangerang"
-// dimana nama sebagai key dan "Bagus" sebagai value
-// let dataPersonal1 = {
-//   nama: "Bagus",
-//   alamat: "Tangerang",
-//   umur: 24,
-// };
-
-// let dataPersonal2 = {
-//   nama: "Guswandi",
-//   alamat: "Padang",
-//   umur: 20,
-// };
-
-// let dataPersonal3 = {
-//   nama: "Arsya",
-//   alamat: "Karawang",
-//   umur: 17,
-// };
-
-// console.log(dataPersonal1);
-// console.log(dataPersonal2.nama);
-// console.log(dataPersonal3.umur);
-
-// Array of object
-// Memperbolehkan kita menyimpan lebih dari satu object dalam satu variabel
-// let dataPersonal = [
-//   {
-//     nama: "Bagus",
-//     alamat: "Tangerang",
-//     umur: 24,
-//   },
-//   {
-//     nama: "Guswandi",
-//     alamat: "Padang",
-//     umur: 20,
-//   },
-//   {
-//     nama: "Arsya",
-//     alamat: "Karawang",
-//     umur: 17,
-//   },
-// ];
-
-// console.log(dataPersonal);
-// console.log(dataPersonal[1]);
-// console.log(dataPersonal[2].nama);
-
-// let data = [];
-
-// function addData(event) {
-//   // preventDefault
-//   // untuk menghindari html menjalankan fungsi yang ia bawa, sebagai contoh refresh page
-//   event.preventDefault();
-
-//   let person = {
-//     nama: "Maulana",
-//     alamat: "Bandung",
-//     umur: 24,
-//   };
-
-//   // fungsi push adalah memasukkan data baru di dalam sebuah array
-//   // sebagai contoh, data person akan dimasukkan ke dalam array data
-//   data.push(person, person);
-
-//   console.log(data);
-// }
-
-// let data = [];
-
-// function addData(event) {
-//   event.preventDefault();
-
-//   let blog = {
-//     title: document.getElementById("input-blog-title").value,
-//     content: document.getElementById("input-blog-content").value,
-//   };
-
-//   data.push(blog);
-//   console.log(data);
-// }
-
-let dataBlog = [];
+let data = [];
 
 function addBlog(event) {
   event.preventDefault();
@@ -122,7 +15,7 @@ function addBlog(event) {
     content,
     image,
     postAt: "10 August 2023",
-    author: "Abel Dustiin",
+    author: "Ahmad Yusril",
   };
 
   dataBlog.push(blog);
