eff3e83a37cbae12e7668ec1de4f9cd298d53a03
Yusril Ahmad <yusril7680@gmail.com>
2023-08-11 10:14:29 +0700

    day-5

--- js/blog.js
+++ js/blog.js
@@ -25,12 +25,12 @@ function addBlog(event) {
 }
 
 function renderBlog() {
-  document.getElementById("contents").innerHTML = "";
+  document.getElementById ("name").innerHTML = "";
 
   for (let index = 0; index < dataBlog.length; index++) {
     console.log(dataBlog[index]);
 
-    document.getElementById("contents").innerHTML += `
+    document.getElementById ("name").innerHTML += `
       <div class="blog-list-item">
         <div class="blog-image">
           <img src="${dataBlog[index].img}" alt="" />
@@ -57,10 +57,82 @@ function renderBlog() {
   }
 }
 
-// looping menggunakan for
-// pada () for, value pertama adalah default value
-// value kedua merupakan kondisi
-// value ketiga akan berjalan ketika kondisi value kedua true
-// for (let index = 0; index < dataBlog.length; index++) {
-//   console.log(dataBlog[index]);
-// }
+function getFullTime(time) {
+  // new Date() mendapatkan terkait tanggal dan waktu kapan fungsinya dijalankan
+  // let time = new Date();
+  // console.log(time);
+
+  let monthName = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  // console.log(monthName[0]);
+
+  let date = time.getDate();
+  // console.log(date);
+
+  let monthIndex = time.getMonth();
+  // console.log(monthIndex);
+  // console.log(monthName[monthIndex]);
+
+  let year = time.getFullYear();
+  // console.log(year);
+
+  let hours = time.getHours();
+  let minutes = time.getMinutes();
+
+  if (hours <= 9) {
+    // 09
+    hours = "0" + hours;
+  } else if (minutes <= 9) {
+    minutes = "0" + minutes;
+  }
+
+  // console.log(`${hours}:${minutes}`);
+
+  // 11 Aug 2023 09:18 WIB
+  return `${date} ${monthName[monthIndex]} ${year} ${hours}:${minutes} WIB`;
+}
+
+function getDistanceTime(time) {
+  let timeNow = new Date();
+  let timePost = time;
+
+  let distance = timeNow - timePost; // hasilnya milisecond
+  // console.log(distance);
+
+  let milisecond = 1000; // milisecond
+  let secondInHours = 3600; // 1 jam 3600 Detik
+  let hoursInDays = 24; // 1 Hari 24 Jam
+
+  let distanceDay = Math.floor(
+    distance / (milisecond * secondInHours * hoursInDays)
+  );
+  let distanceHours = Math.floor(distance / (milisecond * 60 * 60));
+  let distanceMinutes = Math.floor(distance / (milisecond * 60));
+  let distanceSecond = Math.floor(distance / milisecond);
+
+  if (distanceDay > 0) {
+    return `${distanceDay} days ago`;
+  } else if (distanceHours > 0) {
+    return `${distanceHours} hours ago`;
+  } else if (distanceMinutes > 0) {
+    return `${distanceMinutes} minutes ago`;
+  } else {
+    return `${distanceSecond} seconds ago`;
+  }
+}
+
+setInterval(function () {
+  renderBlog();
+}, 3000);
